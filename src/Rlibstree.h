3fdeff984fdcc0f7c4a8ffdc95a75012f170ef91
Wush Wu <wush978@gmail.com>
2012-08-25 20:32:10 +0800

    create stree

---
+++ src/Rlibstree.h
@@ -0,0 +1,22 @@
+/*
+ * Rlibstree.h
+ *
+ *  Created on: Aug 25, 2012
+ *      Author: wush
+ */
+
+#ifndef RLIBSTREE_H_
+#define RLIBSTREE_H_
+
+#include <memory>
+
+#include <Rcpp.h>
+
+#include "libstree.h"
+
+#include "Tree.h"
+
+RcppExport SEXP Rlibstree__new(SEXP Rsrc);
+RcppExport SEXP Rlibstree__new__int(SEXP Rsrc);
+
+#endif /* RLIBSTREE_H_ */
---
+++ src/Tree.cpp
@@ -0,0 +1,23 @@
+/*
+ * Tree.cpp
+ *
+ *  Created on: Aug 25, 2012
+ *      Author: wush
+ */
+
+#include "Tree.h"
+
+namespace Rlibstree {
+
+Tree::Tree(void *data, u_int item_size, u_int num_items)
+: src(lst_string_new(data, item_size, num_items)), set(lst_stringset_new()), tree(NULL)
+{
+	lst_stringset_add(set, src);
+	tree = lst_stree_new(set);
+}
+
+Tree::~Tree() {
+	lst_stree_free(tree);
+}
+
+} /* namespace Rlibstree */
